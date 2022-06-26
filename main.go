9f09466dcaa00e64de247afc426b4912bdf5de95
Gyejin Lee <gyejin.lee@konglabs.net>
2022-06-26 22:50:09 +0900

    modify: Introduction of variables, and functions in Go

--- main.go
+++ main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	var whatToSay string = "Hello, world!"
+	var i int = 1
+
+	whatToSay = "Goodbye, cruel world!"
+
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to ", i)
+	
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+
+	fmt.Println("The function return", whatWasSaid, theOtherThingThatWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "Soemthing", "else"
 }
